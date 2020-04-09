60a778241ff863a2e8ead888265410167e6a32bd
Retr00.exe <55347344+retr00exe@users.noreply.github.com>
2020-04-09 23:22:19 +0700

    Update wordlist.py

--- wordlist.py
+++ wordlist.py
@@ -1,22 +1,34 @@
 import subprocess as sp
+import os
 
-word = input("[*] Enter word : ")
-length = int(input("[*] Input the length : "))
+welcome = """                    
+██╗    ██╗ ██████╗ ██████╗ ██████╗ ██╗     ██╗███████╗████████╗     ██████╗ ███████╗███╗   ██╗███████╗██████╗  █████╗ ████████╗ ██████╗ ██████╗ 
+██║    ██║██╔═══██╗██╔══██╗██╔══██╗██║     ██║██╔════╝╚══██╔══╝    ██╔════╝ ██╔════╝████╗  ██║██╔════╝██╔══██╗██╔══██╗╚══██╔══╝██╔═══██╗██╔══██╗
+██║ █╗ ██║██║   ██║██████╔╝██║  ██║██║     ██║███████╗   ██║       ██║  ███╗█████╗  ██╔██╗ ██║█████╗  ██████╔╝███████║   ██║   ██║   ██║██████╔╝
+██║███╗██║██║   ██║██╔══██╗██║  ██║██║     ██║╚════██║   ██║       ██║   ██║██╔══╝  ██║╚██╗██║██╔══╝  ██╔══██╗██╔══██║   ██║   ██║   ██║██╔══██╗
+╚███╔███╔╝╚██████╔╝██║  ██║██████╔╝███████╗██║███████║   ██║       ╚██████╔╝███████╗██║ ╚████║███████╗██║  ██║██║  ██║   ██║   ╚██████╔╝██║  ██║
+ ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚══════╝╚═╝╚══════╝   ╚═╝        ╚═════╝ ╚══════╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝
+By Mekel Ilyasa
+github.com/retr00exe
+"""
+print(welcome)
 
-file = open(word + '.txt', "w")
-file.close() 
+wordList = input("[*] Enter list of word : ").split()
+length = int(input("[*] Enter the number of digits : "))
+print()
 
-totalLength = len(word) + length
-padding = 0
 file = ['cat']
 
-for i in range(length):
-    pattern = word + '@' * (length - padding)
-    fileOutput = word + str(padding) + '.txt'
-    cmd = ['crunch',str(totalLength-padding),str(totalLength-padding),'1234567890','-t',pattern,'-o',fileOutput]
-    padding = padding + 1
-    file.append(fileOutput)    
-    sp.call(cmd)
-                                
+for word in wordList:
+    padding = 0
+    totalLength = len(word) + length
+    for i in range(length):
+        pattern = word + '@' * (length - padding)
+        fileOutput = word + str(padding) + '.txt'
+        cmd = ['crunch',str(totalLength-padding),str(totalLength-padding),'1234567890','-t',pattern,'-o',fileOutput]
+        padding = padding + 1
+        file.append(fileOutput)    
+        print(cmd)
+    
 file.extend(['>',word + '.txt'])
-sp.call(file)
+print(file)
